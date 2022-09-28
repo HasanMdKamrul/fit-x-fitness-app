d9345100aa8e5e7b87dc546d159c9f63bdd22f4b
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-28 19:57:52 +0200

    AddABreak component added

--- src/components/AddABreak/AddABreak.js
+++ src/components/AddABreak/AddABreak.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const AddABreak = () => {
     return (
-        <div>
-            <h1>Add A Break</h1>
+        <div className='flex justify-center mt-5'>
+           <div className='border px-8 py-6 shadow-xl rounded-2xl'>
+                <h1 className='text-3xl mb-2'>Add A Break</h1>
+                <div className="btn-group">
+                    <button className="btn btn-lg">1</button>
+                    <button className="btn btn-lg btn-active">2</button>
+                    <button className="btn btn-lg">3</button>
+                    <button className="btn btn-lg">4</button>
+                </div>
+            </div>
         </div>
     );
 };
