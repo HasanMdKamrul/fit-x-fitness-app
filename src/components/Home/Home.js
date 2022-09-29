7c246c2e9f8a8149b60446353a20c17016ca243d
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-29 04:18:40 +0200

    Made the website responsive for all 3 devices and final commit added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -39,15 +39,15 @@ const Home = () => {
 
     return (
         <div>
-            <div className='lg:grid lg:grid-cols-12 grid grid-cols-1 mx-5'>
-                <div className='lg:col-start-1 lg:col-end-9 col-start-1 col-end-12 lg:border-r p-5'>
-                    <div className='lg:grid lg:grid-cols-3 lg:gap-2 grid grid-cols-1 lg:mx-12 lg:p-0 px-5 mt-12'>
+            <div className='lg:grid lg:grid-cols-12 md:grid md:grid-cols-12 grid grid-cols-1 mx-5'>
+                <div className='lg:col-start-1 lg:col-end-9 md:col-start-1 md:col-end-6 col-start-1 col-end-12 lg:border-r p-5'>
+                    <div className='lg:grid lg:grid-cols-3 lg:gap-2 md:grid md:grid-cols-2 md:gap-5 grid grid-cols-1 lg:mx-12 lg:p-0 px-5 mt-12'>
                         {
                             activities.map(activity => <Activity handleAddToList={handleAddToList}  key={activity._id} activity={activity}/>)
                         }
                     </div>
                 </div>
-                <div className='lg:col-start-10 lg:col-end-12 col-start-1 col-end-12 mt-5 p-5'>
+                <div className='lg:col-start-10 lg:col-end-12 col-start-1 md:col-start-7 md:col-end-12 col-end-12 mt-5 p-5'>
                     <ActivityMonitor duration={duration}></ActivityMonitor>
                 </div>
             </div>
